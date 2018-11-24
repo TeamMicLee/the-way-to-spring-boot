b47fdd55b60fbc7a6377620cc7a1378142ef30fb
litao <TeamMicLee@foxmail.com>
2018-11-24 14:03:17 +0800

    spring-boot工程

---
+++ src/main/java/com/lite7/demo/domain/User.java
@@ -0,0 +1,37 @@
+package com.lite7.demo.domain;
+
+/**
+ * @Author: Lite7
+ * @Date: 2018/11/24 10:51 AM
+ * @Version 1.0
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
