d7c3a88797fd6bce86bdf82a754472a6f9a3e00f
litao <TeamMicLee@foxmail.com>
2018-11-26 10:25:37 +0800

    添加 swagger-ui 配置

--- src/main/java/com/lite7/demo/web/HelloController.java
+++ src/main/java/com/lite7/demo/web/HelloController.java
@@ -1,14 +1,20 @@
 package com.lite7.demo.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "https://teammiclee.github.io/");
+        map.addAttribute("name", "Lite.7");
+        map.addAttribute("age", "25");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
     }
 
 }
